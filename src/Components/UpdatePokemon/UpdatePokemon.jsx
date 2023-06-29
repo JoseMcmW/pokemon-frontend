ca000509693a62ed42d65d935d89a729ba2db1f1
Jose Centeno <josemanuelcm25@gmail.com>
2023-06-29 13:25:06 -0300

    deploy

--- src/Components/UpdatePokemon/UpdatePokemon.jsx
+++ src/Components/UpdatePokemon/UpdatePokemon.jsx
@@ -111,7 +111,7 @@ const UpdatePokemon = () => {
       weight: 0,
       types: [],
     });
-    window.location.href = "https://pokemon-backend-production-74d4.up.railway.app/home";
+    window.location.href = "https://pokemon-frontend-flax.vercel.app/home";
   };
 
   //Handler del botón para eliminar types seleccionados.
